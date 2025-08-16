28699e5c2a9ef0143edf6905537002a6990cba7d
PiyushGhavghave <piyushghavghave@gmail.com>
2025-08-17 04:52:09 +0530

    fixed: Volume vs service level component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,6 +7,7 @@ import VisitorInsights from './VisitorInsights';
 import TotalRevenue from './TotalRevenue';
 import CustomerSatisfaction from './CustomerSatisfaction';
 import TargetVsReality from './TargetVsReality';
+import VolumeServiceLevel from './VolumeServiceLevel';
 
 const Dashboard = () => {
   return (
@@ -33,18 +34,7 @@ const Dashboard = () => {
           <div className="products-section">
             <TopProducts />
           </div>
-          <div className="service-level-section">
-            <Chart
-              title="Volume vs Service Level"
-              type="service-level"
-              subtitle1="Volume"
-              subtitle2="Services"
-              value1="2.6K"
-              value2="1.2K"
-              data={[8, 10, 9, 7, 6, 8, 5]}
-              colors={["#5570FF", "#00D4AA"]}
-            />
-          </div>
+          <VolumeServiceLevel />
         </div>
       </div>
     </div>
